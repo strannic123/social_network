4a4227f6d7eb55e6f2ec7340871b6b220e993b79
strannic123 <manager350@mail.ru>
2020-08-18 19:30:16 +0300

    only commit

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./app-header.css";
+
+const AppHeader = ({liked, allPosts}) => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Иван Иванов</h1>
+            <h2>{allPosts} записей, понравилось {liked}</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
